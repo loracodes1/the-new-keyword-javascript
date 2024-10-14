1b95f90d72c3570c3354aedc60c6729ceb21bd19
florence <loracodes3@gmail.com>
2024-10-14 12:25:46 +0300

    commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // The new keyword in JavaScript is used to create an instance of an object that has a specific constructor.
 
+// 1.Creating Instances of Objects
+// When you use the new keyword with a constructor function or a class, it creates a new object and sets up the prototype chain.
+function Dog(name) {
+    this.name = name;
+    this.bark = function() {
+        console.log(`${this.name} says Woof!`);
+    };
+}
+
+const myDog = new Dog('Buddy');
+myDog.bark(); // Output: Buddy says Woof!
